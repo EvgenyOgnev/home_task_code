8aa3d4e66456eca6fbcc06e58ff6ee0b1eb7d0c5
Evgeny Ognev <evgenyiognev-nn@mail.ru>
2022-07-30 12:48:49 +0300

    задача 23

---
+++ task_23/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число N: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = new int[size];
+Console.Write($"кубы чисел от 1 до {size} ->> ");
+for(int i = 0; i < size; i++)
+{
+    int j = i + 1;
+    array[i] = j*j*j;
+    Console.Write(array[i] + ", ");
+}
+
